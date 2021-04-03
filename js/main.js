5e3863d76260fa587813d22a1fe5f3a3522bde40
Dbirtic <Dominik.birtic666@gmail.com>
2021-04-03 21:53:09 +0200

    Finished the pdf viewer

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const renderPage = (num) => {
     // Get page
     pdfDoc.getPage(num).then((page) => {
         // Set scale
-        const viewport = page.getViewport({scale});
+        const viewport = page.getViewport({ scale });
         canvas.height = viewport.height;
         canvas.width = viewport.width;
 
@@ -28,7 +28,7 @@ const renderPage = (num) => {
         page.render(renderCtx).promise.then(() => {
             pageIsRendering = false;
 
-            if(pageIsRendering !== null){
+            if(pageNumIsPending !== null){
                 renderPage(pageNumIsPending);
                 pageNumIsPending = null;
             }
@@ -37,6 +37,33 @@ const renderPage = (num) => {
         // Output current page
         document.querySelector("#page-num").textContent = num;
     });
+};
+
+// Check for pages rendering
+const queueRenderPage = num => {
+    if(pageIsRendering){
+        pageNumIsPending = num;
+    } else{
+        renderPage(num);
+    }
+};
+
+// Show Previous Page
+const showPrevPage = () =>{
+    if(pageNum <= 1){
+        return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+}
+
+// Show Next Page
+const showNextPage = () =>{
+    if(pageNum >= pdfDoc.numPages){
+        return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
 }
 
 // Get Document
@@ -47,4 +74,17 @@ pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
     document.querySelector("#page-count").textContent = pdfDoc.numPages;
 
     renderPage(pageNum);
-});
+})
+    .catch(err => {
+        const div = document.createElement('div');
+        div.className = 'error';
+        div.appendChild(document.createTextNode(err.message));
+        document.querySelector('body').insertBefore(div, canvas);
+
+        // remove top bar
+        document.querySelector('.top-bar').style.display = 'none';
+    });
+
+// Button Event
+document.querySelector("#prev-page").addEventListener('click', showPrevPage);
+document.querySelector("#next-page").addEventListener('click', showNextPage);
